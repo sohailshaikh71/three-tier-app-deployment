20ad42fd7b613c85b7ee348c334c6b99362771fd
sohailshaikh71 <156609462+sohailshaikh71@users.noreply.github.com>
2025-10-29 19:18:31 +0530

    Update database name in DbConfig.js
    updated

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     DB_HOST : 'database-1-instance-1.c0ryiss06l9v.us-east-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'sohail123',
-    DB_DATABASE : 'databas-1'
+    DB_DATABASE : 'webappdb'
 });
